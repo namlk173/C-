fa338d45bedc44ab32f3ddbc772044b7377d6de9
namlk173 <lekhacnam1703@gmail.com>
2023-05-10 22:19:57 +0700

    Bao Cao CTDL Tong Hop

--- DSLK_Don.cpp
+++ DSLK_Don.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+int n, x;
+	
 struct Node {
 	int data;
 	struct Node *next;
@@ -9,50 +10,41 @@ struct Node {
 struct Node * head = NULL;
 
 void insert(int new_data){
-//	struct Node * new_node = (struct Node*) malloc(sizeof(struct Node));
 	struct Node *ptr;
-	ptr = head;
-	while(ptr != NULL){
-		ptr = ptr->next;
-	}
 	Node *new_node = new Node;
 	new_node->data = new_data;
-	ptr->next = new_node;
 	new_node->next = NULL;
-	if(head->next==NULL) {
+	ptr = head;
+	if(ptr == NULL){
 		head = new_node;
+	} else {
+		while(ptr->next != NULL){
+			ptr = ptr->next;
+		}
+		ptr->next= new_node;
 	}
 }
-
-//void delete_value(int value){
-//	Node *cur = new Node;
-//	Node *pre = new Node;
-//	cur = head;
-//	for(int i=1; i<pos; i++){
-//		pre = cur;
-//		cur = cur->next;
-//	}
-//	pre->next = cur->next;
-//}
 
 
 void display(){
 	struct Node *ptr;
 	ptr = head;
 	while(ptr != NULL){
-		cout<<ptr->data<<" ";
+		if(ptr->data!=x){
+			cout<<ptr->data<<" ";
+		}
 		ptr = ptr->next;
 	}
 	cout<<endl;
 }
 
 int main(){
-	int n, x;
 	cin>>n;
 	for(int i=0; i<n; i++){
 		cin>>x;
 		insert(x);
 	}
+	cin>>x;
 	display();
 	return 0;
 }
